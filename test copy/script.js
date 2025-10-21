eaa1ad61800cff77de7a212d9db4b1483308d61a
Aradhana01Gambhir <gambhiraradhana8@gmail.com>
2025-10-21 21:24:38 +0530

    Update script.js

--- test copy/script.js
+++ test copy/script.js
@@ -1,6 +1,5 @@
 // --- Backend API URL ---
-const API_URL = 'http://127.0.0.1:5001';
-
+const API_URL = 'https://sahayata-ai-backend.onrender.com';
 // --- Page Navigation ---
 const dashboardPage = document.getElementById('dashboard-page');
 const activityPage = document.getElementById('activity-page');
